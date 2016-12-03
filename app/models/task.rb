9c97b51c00912df3484a25f157112d68f34dded5
lauritha <lizamavergaralaura@gmail.com>
2016-12-03 19:05:21 -0300

    modifico scope a false

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  scope :completed, -> { where(is_done: true)}
+  scope :completed, -> { where(is_done: false)}
   scope :start_with, -> (word) { where("name like ?", "#{word}%") }
 
   belongs_to :account
